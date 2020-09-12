b05548b87b60a7f209c78c3b2b8f4a171b0d28de
Kristin Yates <kyates81@gmail.com>
2020-09-12 19:39:01 -0400

    Create CRUD operations and pages for leave type, add FontAwesome and DataTables

---
+++ leave-management/Controllers/LeaveTypeController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using leave_management.Contracts;
+using leave_management.Data;
+using leave_management.Models;
+using leave_management.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leave_management.Controllers
+{
+    public class LeaveTypeController : Controller
+    {
+        private readonly ILeaveTypeRepository _repo;
+        private readonly IMapper _mapper;
+        public LeaveTypeController(ILeaveTypeRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+
+        }
+
+        // GET: LeaveTypeController
+        public ActionResult Index()
+        {
+            var leaveTypes = _repo.FindAll();
+            var model = _mapper.Map<ICollection<LeaveType>,ICollection<LeaveTypeViewModel>>(leaveTypes);
+            return View(model);
+        }
+
+        // GET: LeaveTypeController/Details/5
+        public ActionResult Details(int id)
+        {
+            if (!_repo.isExists(id))
+            {
+                return NotFound();
+            }
+
+            var leaveType = _repo.FindById(id);
+            var model = _mapper.Map<LeaveTypeViewModel>(leaveType);
+            return View(model);
+        }
+
+        // GET: LeaveTypeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveTypeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(LeaveTypeViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var leaveType = _mapper.Map<LeaveType>(model);
+                leaveType.DateCreated = DateTime.Now;
+
+                var isSuccess = _repo.Create(leaveType);
+
+                if (!isSuccess)
+                {
+                    ModelState.AddModelError("", "Something Went Wrong...");
+                    return View(model);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveTypeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (!_repo.isExists(id))
+            {
+                return NotFound();
+            }
+
+            var leaveType = _repo.FindById(id);
+            var model = _mapper.Map<LeaveTypeViewModel>(leaveType);
+            return View(model);
+        }
+
+        // POST: LeaveTypeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, LeaveTypeViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var leaveType = _mapper.Map<LeaveType>(model);
+
+                var isSuccess = _repo.Update(leaveType);
+
+                if (!isSuccess)
+                {
+                    ModelState.AddModelError("", "Update Failed");
+                    return View(model);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Update Failed");
+                return View(model);
+            }
+        }
+
+        // GET: LeaveTypeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var leaveType = _repo.FindById(id);
+                if (leaveType == null)
+                {
+                    return NotFound();
+                }
+                var isSuccess = _repo.Delete(leaveType);
+
+                if (!isSuccess)
+                {
+                    return BadRequest();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return BadRequest();
+
+            }
+        }
+
+    }
+}
